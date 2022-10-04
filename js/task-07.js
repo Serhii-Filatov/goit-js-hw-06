fa5de84fd1828a81ab5bee44d8fd9d9d4bc7c1b1
Serhii-Filatov <55346150+Serhii-Filatov@users.noreply.github.com>
2022-10-04 18:08:49 +0300

    Update task-7

--- js/task-07.js
+++ js/task-07.js
@@ -3,13 +3,10 @@
     (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. 
     В результате при перетаскивании ползунка будет меняться размер текста.
 */
-const refs = {
-  input: document.querySelector('#font-size-control'),
-  span: document.querySelector('#text'),
-}
 
-const changeTextSize = (event) => {
-  refs.span.style.fontSize = `${event.currentTarget.value}px`;
-}
-
-refs.input.addEventListener('input', changeTextSize)
+const inputTextRef = document.querySelector('#text');
+const inputControlRef = document.querySelector('#font-size-control');
+inputControlRef.oninput = function () {
+  console.log(this.value);
+  inputTextRef.style.fontSize = inputControlRef.value + 'px';
+};
